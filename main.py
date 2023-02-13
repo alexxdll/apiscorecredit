24dabfd4dca43c6edca9dff6c12660689c38a91e
Alexandre Delaguillaumie <alexandre.delaguillaumie@gmail.com>
2023-02-13 16:24:02 +0100

    reinit du main.py avec uvicorn run

--- main.py
+++ main.py
@@ -37,5 +37,5 @@ async def shap_values(client_id : int):
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
-#if __name__ == '__main__':
-#    uvicorn.run(app, host='127.0.0.1', port=8000)
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
